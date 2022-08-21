c6bfd1010e11c0548945e32d1a41949b34eba6cc
Jan <jan.gombos@student.tuke.sk>
2022-08-21 18:29:52 +0200

    update for dec

--- app/common/decorators.py
+++ app/common/decorators.py
@@ -26,38 +26,7 @@ def register_route(
                 response=response,
                 description=function.__doc__,
             )
-        function = bp_or_app.route(rule=rule, **options)(function)
+        function = wraps(bp_or_app.route(rule=rule, **options)(function))
         return function
 
     return decorator
-
-""" def register_route(
-    bp_or_app,
-    rule: str,
-    request: Optional[BaseModelDocumentable],
-    response: Optional[BaseModelDocumentable],
-    document: bool = True,
-    **options: Any
-):
-    def decorator(function):
-        @wraps(function)
-        def enhanced_fn():
-            try:
-                prefix = bp_or_app.url_prefix
-                if prefix is None:
-                    prefix = ""
-            except AttributeError:
-                prefix = ""
-            if document:
-                DocumentedApp.add_endpoint(
-                    endpoint=prefix + rule,
-                    request=request,
-                    response=response,
-                    description=function.__doc__,
-                )
-            function = bp_or_app.route(rule=rule, **options)(function)
-            return function
-
-        return enhanced_fn()
-
-    return decorator """
